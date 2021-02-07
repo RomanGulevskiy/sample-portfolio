13ec9c7db342de43358016c7afa24ae577f469c1
Roman Gulevskiy <roman.gulevskiy@gmail.com>
2021-02-08 01:16:03 +0500

    added language links for mobile menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 let hamburgerButton = document.querySelector('.hamburger-button');
 let menu = document.querySelector('.header-nav');
 let menuLink = document.querySelectorAll('.header-menu-link');
+let heroLanguage = document.querySelector('.hero-language');
 
 hamburgerButton.addEventListener('click', function() {
     menu.classList.toggle('mobile');
+    heroLanguage.classList.toggle('mobile');
     hamburgerButton.classList.toggle('hamburger-button-close');
 });
 menuLink.forEach(link => {
     link.addEventListener('click', function() {
         menu.classList.toggle('mobile');
+        heroLanguage.classList.toggle('mobile');
         hamburgerButton.classList.toggle('hamburger-button-close');
     });
 });
